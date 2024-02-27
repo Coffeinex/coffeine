ec7caff8e67ab38cd21336b713cfe65f7085ae6e
Терминал7 <term7@classroom.local>
2024-02-27 12:29:53 +0300

    Initial commit

---
+++ src/main/java/sqr.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class sqr {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        in.close();
+
+        String[] parts = str.split("");
+        String numstr0, numstr1, numstr2;
+        numstr0 = parts[0];
+        numstr1 = parts[1];
+        numstr2 = parts[2];
+        try {
+            int num0 = Integer.parseInt(numstr0);
+            int num2 = Integer.parseInt(numstr2);
+            if (num2 == 0) {
+                System.out.println("Не удалось получить число из выражения");
+                return;
+            }
+            switch (numstr1) {
+                case "+": {
+                    int res = num0 + num2;
+                    System.out.print("Результат: " + res);
+                    break;
+                }
+                case "-": {
+                    int res = num0 - num2;
+                    System.out.print("Результат: " + res);
+                    break;
+                }
+                case "*": {
+                    int res = num0 * num2;
+                    System.out.print("Результат: " + res);
+                    break;
+                }
+                case "/": {
+                    int res = num0 / num2;
+                    System.out.print("Результат: " + res);
+                    break;
+                }
+                default:
+                    System.out.print("Ошибка");
+                    break;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Не удалось получить число из выражения: " + str);
+            return;
+        }
+
+
+
+    }
+}
